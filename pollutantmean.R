0c3dfa33510d18c4093a903a3d6fba2ba5e655f8
eastbourne1904 <alasdair.hazen@gmail.com>
2015-04-11 16:18:25 -0400

    pollutantmean homework

---
+++ pollutantmean.R
@@ -0,0 +1,5 @@
+#h <- "You selected: "
+#polutant <- readline("What is the pollutant you want me to search for? sulfate or nitrate: ")
+#id <- readline("What monitor id do you want me to check? ")
+#print(paste(h,polutant, id))
+directory <- read.csv(file="C:/Users/Alasdair/OneDrive/Data Science Cert/R Programming/Downloads/rprog-data-specdata/specdata/001.csv")
